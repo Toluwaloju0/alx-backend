8af8c1138f75de73a09cef35bf2d841f22387d7b
Toluwaloju0 <teecoded1999@gmail.com>
2024-10-26 21:14:31 +0100

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,13 +4,9 @@
 import csv
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """A function to paginate a page"""
 
-    count: int = 0
-
-    with open("Popular_Baby_Names.csv", 'r') as csv_file:
-        end_page = page_size * page
-        csv_list = list(csv.DictReader(csv_file))
-        return end_page - page_size, end_page
-            
+    end_page = page_size * page
+    return end_page - page_size, end_page
