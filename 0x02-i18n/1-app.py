d27351a10c3eac900f1affe33711ba1661164cba
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-05 21:37:01 +0100

    flask

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """A mnodule to instantiate using Babel"""
 
-app = __import__('0-app').app
 from flask_babel import Babel
 
+app = __import__('0-app').app
 babel = Babel(app)
 
 
@@ -16,12 +16,13 @@ class Config:
     def locale():
         """To set the babel local language"""
 
-        return config.LANGUAGES[0]
+        return Config.LANGUAGES[0]
 
     @babel.timezoneselector
     def timezone():
         """To set the time zone"""
 
         return 'UTC'
+
 
 app.config.from_object('Config')
