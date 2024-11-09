4b86da050aab18c960fb12901f5aecf6bbcd1741
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-09 14:00:02 +0100

    flask babel

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-"""
+
+
 from flask import render_template
 app = __import__('2-app').app
 
-@app.route('/')
+@app.route('/3')
 def get_3():
     return render_template('3-index.html')
-
-if __name__ == '__main__':
-    app.run()"""
