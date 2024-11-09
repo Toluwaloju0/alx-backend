c81253b5c8a82675d6a763b12a92cda19762de99
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-09 05:44:43 +0100

    flask

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -2,5 +2,12 @@
 """A module to force a locale on a web page"""
 
 from flask import request
-app = __import__('1-app').app
+from flask_babel import force_locale
+app = __import__('3-app').app
+get_locale = __import__('2-app').get_locale
 
+locale = get_locale()
+
+if locale == 'fr':
+    with force_locale(locale):
+        return render_template('4-index.html')
